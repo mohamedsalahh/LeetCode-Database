4fe1235bcd9856034e65e01a16cd62dcdd039023
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-07-07 13:57:24 +0200

    Time: 1422 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
 
-SELECT CAST(SUBSTRING(CNCT, 1, LOCATE(',', CNCT)) AS UNSIGNED) AS actor_id, CAST(SUBSTRING(CNCT, LOCATE(',', CNCT)+1) AS UNSIGNED) AS director_id
-FROM ( 
-    SELECT CNCT
-    FROM ( 
-        SELECT CONCAT(actor_id, ',', director_id) AS CNCT
-        FROM ActorDirector
-    ) AS Q1
-    GROUP BY CNCT
-    HAVING COUNT(*) >= 3
-) AS Q2;
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
